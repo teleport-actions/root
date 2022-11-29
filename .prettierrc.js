57513c27c7a4b29755fbe5e3f25556516e22c429
Noah Stride <noah@noahstride.co.uk>
2022-11-29 18:23:11 +0000

    Add root level configuration files for prettier, eslint etc

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
