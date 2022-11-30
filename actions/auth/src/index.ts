e5d93183014545e8286e517d599b549bbc63d765
Noah Stride <noah@noahstride.co.uk>
2022-11-30 10:43:38 +0000

    Add code for auth action

---
+++ actions/auth/src/index.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+
+import * as core from '@actions/core';
+
+import * as tbot from '@root/lib/tbot';
+import * as io from '@root/lib/io';
+
+async function run() {
+  const sharedInputs = tbot.getSharedInputs();
+  const config = tbot.baseConfigurationFromSharedInputs(sharedInputs);
+
+  const destinationPath = await io.makeTempDirectory();
+  config.destinations.push({
+    directory: {
+      path: destinationPath,
+    },
+    roles: [], // Use all assigned to bot,
+  });
+
+  const configPath = await tbot.writeConfiguration(config);
+  await tbot.execute(configPath);
+
+  const identityFilePath = path.join(destinationPath, 'identity');
+  core.setOutput('identity-file', identityFilePath);
+  core.exportVariable('TELEPORT_PROXY', sharedInputs.proxy);
+  core.exportVariable('TELEPORT_AUTH_SERVER', sharedInputs.proxy);
+  core.exportVariable('TELEPORT_IDENTITY_FILE', identityFilePath);
+}
+run().catch(core.setFailed);
